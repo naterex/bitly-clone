3621a46edcf7e1ae17fe7caddc4ba0f2f8e6b0bb
Nate <Welling>
2016-02-03 10:28:33 +0800

    add validation, error message displaying

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,5 @@
 get '/' do
-  @title = "this is the title"
+  @title = "Shorten a URL here"
   @urls = Url.all
   puts "[LOG] Getting /"
   puts "[LOG] Params: #{params.inspect}"
@@ -8,14 +8,27 @@ end
 
 post '/urls' do
   # byebug
+  puts "[LOG] Getting /urls"
   puts "[LOG] Params: #{params.inspect}"
-  Url.create(long_url: params[:long_url])
-  redirect '/'
+  @url = Url.new(long_url: params[:long_url])
+  if @url.save
+    redirect '/'
+  else
+    @errors = @url.errors.messages[:long_url].to_s
+    byebug
+    # @errors = @errors.to_s
+    redirect "/?errors=#{@errors}"
+    # @urls = Url.all
+    # erb :'static/index'
+  end
+  # redirect '/'
 end
 
 #i.e. /q6bda
 get '/:short_url' do
-  url = Url.find_by(short_url: params[:short_url])
-  redirect "http://#{url.long_url}"
+  @url = Url.find_by(short_url: params[:short_url])
+  @url.update_click_count
+  redirect '/'
+  # redirect "http://#{url.long_url}"
   # erb :"static/short_url"
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,8 @@
 class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
-  validates :long_url, uniqueness: true
+  validates :long_url, uniqueness: true, presence: true
+  validates_format_of :long_url, with: /(http:\/\/|https:\/\/)?(www.)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/ix , message: "Error: can't shorten, your URL has incorrect format"
+
   validates :short_url, uniqueness: true
   before_create :shorten
 
@@ -9,4 +11,8 @@ class Url < ActiveRecord::Base
     self.short_url = short_url
   end
 
+  def update_click_count
+    self.click_count += 1
+    self.save
+  end
 end
