f730390d164361fc6fca46a1e787ec21084e31ef
Nate <Welling>
2016-02-04 14:05:13 +0800

    refactor route /:short_url for long/short_url clicks, add index view styling

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,33 +1,49 @@
 
 get '/' do
-  # @title = "SHORTEN. SHARE. MEASURE."
-  @urls = Url.all
   puts "[LOG] Getting /"
   puts "[LOG] Params: #{params.inspect}"
+
+  @site_name = "nate.ly/"
+  @urls = Url.all
+  @urls = @urls.sort_by{ |attribute| attribute[:id] }.reverse # show last added URL at top of list
   erb :"static/index"
 end
 
 post '/urls' do
-  # byebug
   puts "[LOG] Getting /urls"
   puts "[LOG] Params: #{params.inspect}"
+
   @url = Url.new(long_url: params[:long_url])
+
   if @url.save
     redirect '/'
   else
     @errors = @url.errors.messages[:long_url].first
-    # byebug
     redirect "/?errors=#{@errors}"
   end
 end
 
 #i.e. /q6bda
 get '/:short_url' do
-  @url = Url.find_by(short_url: params[:short_url])
-  if @url.present?
-    @url.update_click_count
-    redirect "#{@url.long_url}"
+  puts "[LOG] Getting /:short_url"
+  puts "[LOG] Params: #{params.inspect}"
+
+  @url = Url.find_by(short_url: params[:short_url]) || Url.find_by(long_url: params[:short_url]) # find by short_url or long_url
+
+  if @url.present? # if url found
+
+    if @url.short_url == params['short_url'] # if short_url found
+      @url.update_click_count
+    end
+
+    if @url.long_url.scan(/(http:\/\/|https:\/\/)/).any? # if long_url has HTTP/HTTPS
+      redirect "#{@url.long_url}"
+    else
+      redirect "http://#{@url.long_url}" # append HTTP to long_url
+    end
+
   else
-    redirect "#{@url.long_url}" #
+    redirect '/'
   end
 end
+
--- app/models/url.rb
+++ app/models/url.rb
@@ -2,7 +2,7 @@ class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
   validates_presence_of :long_url, message: "Error: URL can't be blank, please try again."
   validates_uniqueness_of :long_url, message: "Error: URL has already been shortened, please try again."
-  validates_format_of :long_url, with: /(http:\/\/|https:\/\/)(www.)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/ix , message: "Error: incorrect URL format, remember to include http:// or https://"
+  validates_format_of :long_url, with: /(http:\/\/|https:\/\/)?(www.)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/ix , message: "Error: incorrect URL format, remember to include http:// or https://"
 
   validates_uniqueness_of :short_url
   before_create :shorten
